61bc2199b192124115bf96ff21ba26a81643a565
IHiz06 <idelacruz@solintels.com>
2025-01-15 20:44:05 -0500

    meninicial

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+// eslint.config.js
+// @ts-ignore
+import antfu from '@antfu/eslint-config';
+
+export default antfu( {
+  react: true,
+} )
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// Escribo rfce para crear el componente
+//import React from 'react'
+//jsx es una extension de javascript que permite escribir html en javascript
+
+function App() {
+  return (//Solo se debe retornar un solo elemento padre
+    <div> 
+      <h1>Hola Mundo</h1>
+      <p>Hola soy un parrafo xD</p>
+    </div>
+  )
+}
+
+export default App
